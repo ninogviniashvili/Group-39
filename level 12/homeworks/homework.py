e5c01e060950603997a716f8de1073dc47a73876
ninogviniashvili <nino201224@gmail.com>
2025-06-20 22:35:01 +0400

    Add files via upload

---
+++ level 12/homeworks/homework.py
@@ -0,0 +1,19 @@
+#1. პითონის ფაილში ჩამოწერეთ თქვენი სიტყვებით, რა არის sequencing, iteration, selection.
+#2. მომხმარებელს input()-ით შემოატანინე თავისი ასაკი, შემდეგ მამამისის ასაკი და ბოლოს 
+# დაუპრინტე, თუ რამდენჯერ დიდი იქნება მამამისი მასზე 23 წლის შემდეგ.
+
+
+#1) #sequencing -- sequencing aris is rodesac kompiuteri daweril kods zemodan qvemo
+#  kitxulobs
+    #iteration  -- iteration-is dros kodi wreze triaalebs da grdzeldeba usasrulod
+    #selection  -- selectionis dros kompiuteri archevans aketebs ori mxridan tu dawerili ram 
+    # sworia mashin trusken wava da tu arasworia mashin falsisken da im sityvas ityvis romeli 
+    # mxariskenac wava da ra sityvac iqaa chawerili imas gamoitans magram sabolood mainc erti 
+    # pasuxi gamova ekranze
+
+
+#2) 
+
+
+age1= input("your age")
+age2= input("your father's age")
