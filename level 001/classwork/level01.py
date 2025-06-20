0bf489484a5ca4a7674c2d051e385863d811a570
ninogviniashvili <nino201224@gmail.com>
2025-06-20 22:52:39 +0400

    Add files via upload

---
+++ level 001/classwork/level01.py
@@ -0,0 +1,30 @@
+name = "Nino" 
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "Nino" არის ცვლადისთვის მნიშვნელობა
+surname = "Gviniashvili"
+
+# print(name)
+# # პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+# name = "Nino" # ეს არის str(string) ტიპის ცვლადი
+age = 12 # ეს არის int(integer) მთელი რიცხვი
+height = 1.60 # ეს არის float ტიპის ცვლადი (ათწილადი)
+
+knows_programming = True # True ან False
+# is_ugly = False # snakecase(უბრალო წერის სტილი სტანდარტული)
+
+# print(name + " " + surname)
+
+# # სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + age)
+
+# print(type(age)) # age გადაეცა type ფუნქციას, რომელმაც დააბრუნა მისი ტიპი
+# და დაბრუნებული ნებისმიერი სიტყვა დავპრინტეთ, რომელმაც გაგვაგებინა რომ
+# print(type(age)) - ის ტიპი არის int(integer) (მთელი რიცხვი)
+
+print(type(age)) 
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
---
+++ level 001/homework/hw.py
@@ -0,0 +1,8 @@
+name = nino
+lastname = gviniashvili
+age = 12
+height = 1.61
+country = Georgia
+
+
+print('my name is' + name + 'my last name is' + lastname + 'i am' + age + 'years old' + ' and i live in' + country + 'i am ' + height)
