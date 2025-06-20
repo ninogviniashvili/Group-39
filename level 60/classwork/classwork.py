99b42ca6dd722f649af0e5da60fc7833b98886de
ninogviniashvili <nino201224@gmail.com>
2025-06-20 22:21:37 +0400

    Add files via upload

---
+++ level 60/classwork/classwork.py
@@ -0,0 +1,10 @@
+#2. შექმნით სია 5 ელემენტით, შემდეგ კი დაპრინტეთ ამ ელემენტების ჯამი, for loop.
+
+
+list = [5, 1, 6, 7, 9,] 
+
+total = 0
+
+for num in list:
+    total = total + num
+print(total)
